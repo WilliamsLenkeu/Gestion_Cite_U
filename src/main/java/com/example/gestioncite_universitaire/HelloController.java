f2e6531eb3f95008cb9b916fb2dfc6ea98187915
WallyDev <williams25prince@gmail.com>
2023-12-11 19:30:41 +0100

    update

---
+++ src/main/java/com/example/gestioncite_universitaire/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.gestioncite_universitaire;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
