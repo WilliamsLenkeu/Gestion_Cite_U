ead6fefbce3694a9916ee5895bfc2cf547b2ce5c
WallyDev <williams25prince@gmail.com>
2023-12-12 06:06:08 +0100

    update

--- src/main/java/com/example/gestioncite_universitaire/HelloController.java
+++ src/main/java/com/example/gestioncite_universitaire/HelloController.java
@@ -2,6 +2,7 @@ package com.example.gestioncite_universitaire;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 
 public class HelloController {
     @FXML
@@ -10,5 +11,17 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private ToggleButton toggleButton;
+
+    @FXML
+    private void onToggleThemeButtonClick() {
+        if (toggleButton.isSelected()) {
+            welcomeText.setStyle("-fx-text-fill: white; -fx-background-color: darkgray;");
+        } else {
+            welcomeText.setStyle("-fx-text-fill: black; -fx-background-color: lightgray;");
+        }
     }
 }
